b19e059da86e55003ef83d4afe26cbb4d7e7514b
wasurveillancewatch <wasurveillancewatch@gmail.com>
2026-02-01 16:06:13 -0800

    sync saturday noon

---
+++ license_pdf_generator.py
@@ -0,0 +1,7 @@
+"""Stub for license PDF generator - not yet implemented"""
+
+class LicensePDFGenerator:
+    pass
+
+def create_license_pdf_from_callsign(conn, callsign):
+    raise NotImplementedError("PDF generation is not yet implemented")
